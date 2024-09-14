2637ff7765a6fffb90f99304558e9abd8e71262d
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-09-14 15:15:43 +0300

    status updates

--- templates/index.js
+++ templates/index.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    window.nodes = [];
-    window.networks = {
-        "mainnet": true,
-        "testnet-10": true,
-        "testnet-11": true,
+    window.resolver = {
+        sort : "network",
+        nodes : [],
+        networks : {
+            "mainnet": true,
+            "testnet-10": true,
+            "testnet-11": true,
+        },
     };
 
     table = document.createElement('table');
@@ -11,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     thead = document.createElement('thead');
     table.appendChild(thead);
-    thead.innerHTML = "<tr><th>SID:UID</th><th>SERVICE</th><th>VERSION</th><th>FQDN</th><th>PROTO</th><th>ENCODING</th><th>NETWORK</th><th>STATUS</th><th class='right'>PEERS</th><th class='right'>CLIENTS/CAP</th><th class='right'>LOAD</th></tr>";
+    thead.innerHTML = "<tr><th>SID:UID</th><th>SERVICE</th><th>VERSION</th><th class='fqdn'>FQDN</th><th>PROTO</th><th>ENCODING</th><th>NETWORK</th><th>STATUS</th><th class='right'>PEERS</th><th class='right'>CLIENTS/CAP</th><th class='right'>LOAD</th></tr>";
 
     tbody = document.createElement('tbody');
     tbody.id = "nodes";
@@ -23,6 +26,28 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    document.getElementById('fqdn').addEventListener('change', (e) => {
+        let checked = e.target.checked;
+        if (!e.target.checked) {
+            document.getElementById("fqdn-style").innerHTML = ".fqdn { display: none; }";
+        } else {
+            document.getElementById("fqdn-style").innerHTML = "";
+        }
+
+        // Array.from(document.getElementsByClassName('fqdn')).forEach((el) => {
+        //     el.classList.toggle('hidden');
+        // });
+    });
+
+    ["sort-fqdn", "sort-sid", "sort-network"].forEach((id) => {
+        document.getElementById(id).addEventListener('change', (e) => {
+            window.resolver.sort = id.split('-')[1];
+            render();
+        });
+    });
+
+    document.getElementById(`sort-${window.resolver.sort}`).checked = true;
+
     fetchData();
 });
 
@@ -30,7 +55,7 @@ function fetchData() {
     fetch('/status/json')
         .then(response => response.json())
         .then(data => {
-            window.nodes = data;
+            window.resolver.nodes = data;
             render();
             setTimeout(fetchData, 5000);
         })
@@ -41,7 +66,7 @@ function fetchData() {
 }
 
 function filter(node, ctx) {
-    if (!window.networks[node.network]) {
+    if (!window.resolver.networks[node.network]) {
         return "hidden";
     } else if (node.status == "offline" && !ctx.offline) {
         return "hidden";
@@ -49,6 +74,19 @@ function filter(node, ctx) {
         return "hidden";
     } else {
         return node.status;
+    }
+}
+
+function sortData(node) {
+    switch (window.resolver.sort) {
+        case "fqdn":
+            return node.fqdn;
+        case "network":
+            return node.network;
+        case "sid":
+            return node.sid;
+        default:
+            return node.fqdn;
     }
 }
 
@@ -62,19 +100,25 @@ function render() {
     let resort = false;
     let tbody = document.getElementById("nodes");
 
-    const status = nodes
+    const status = window.resolver.nodes
         .filter((node) => node.status === 'online')
         .reduce((acc, node) => {
             const network = node.network;
             if (!acc[network]) {
-                acc[network] = { clients: 0, capacity: 0 };
+                acc[network] = { clients: 0, capacity: 0, count : 0 };
             }
             acc[network].clients += node.clients;
             acc[network].capacity += node.capacity;
+            acc[network].count += 1;
             return acc;
         }, {});
 
-    window.nodes.forEach((node) => {
+    if (window.resolver.sort != window.resolver.lastSort) {
+        resort = true;
+        window.resolver.lastSort = window.resolver.sort;
+    }
+
+    window.resolver.nodes.forEach((node) => {
         let { 
             version,
             sid,
@@ -99,17 +143,20 @@ function render() {
         if (!el) {
             el = document.createElement('tr');
             el.id = uid;
-            el.setAttribute('data-sort', fqdn);
             tbody.appendChild(el);
             resort = true;
         }
         el.className = filter(node, ctx);
 
+        if (resort) {
+            el.setAttribute('data-sort', sortData(node));
+        }
+
         let load = (clients / capacity * 100.0).toFixed(2);
         let peers_ = peers.toLocaleString();
         let clients_ = clients.toLocaleString();
         let capacity_ = capacity.toLocaleString();
-        el.innerHTML = `<td>${sid}:${uid}</td><td>${service}</td><td>${version}</td><td>${fqdn}</td><td>${protocol}</td><td>${encoding}</td><td>${network}</td><td>${status}</td>`;
+        el.innerHTML = `<td>${sid}:${uid}</td><td>${service}</td><td>${version}</td><td class='fqdn'>${fqdn}</td><td>${protocol}</td><td>${encoding}</td><td>${network}</td><td>${status}</td>`;
         if (status != "offline") {
             el.innerHTML += `<td class='wide right'>${peers_}</td><td class='wide right'>${clients_} / ${capacity_}</td><td class='wide right'>${load}%</td>`;
         }
@@ -130,20 +177,21 @@ function render() {
             el.innerHTML = `<input type="checkbox" id="${network}-filter" checked> <label for="${network}-filter">${network}: <span id='${network}-data'></span></label>&nbsp;&nbsp;&nbsp;`;
             tbody.appendChild(el);
 
-            if (window.networks[network] == undefined) {
-                window.networks[network] = true;
+            if (window.resolver.networks[network] == undefined) {
+                window.resolver.networks[network] = true;
             }
 
             document.getElementById(`${network}-filter`).addEventListener('change', () => {
-                window.networks[network] = document.getElementById(`${network}-filter`).checked;
+                window.resolver.networks[network] = document.getElementById(`${network}-filter`).checked;
                 render();
             });
             el = document.getElementById(`${network}-data`);
         }
         let load = (status.clients / status.capacity * 100.0).toFixed(2);
+        let count = status.count.toLocaleString();
         let clients = status.clients.toLocaleString();
         let capacity = status.capacity.toLocaleString();
-        el.innerHTML = `${clients} / ${capacity} &nbsp;${load}%`;
+        el.innerHTML = `(${count}) ${clients} / ${capacity} &nbsp;${load}%`;
     });
 
 }
